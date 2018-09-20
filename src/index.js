adb6460427b5122cb7bb51a67e5a030658c22865
PresByter <presbyter163@163.com>
2018-09-20 18:20:27 +0800

    antd的bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { Button } from 'antd';
+import { Button, Switch } from 'antd';
 import 'antd/lib/button/style/css';
 import styles from './index.css'
 import Pic from './g.jpg'
@@ -11,6 +11,7 @@ ReactDom.render(
     <h1 className={styles.green}>hello, world!333</h1>
     <img src={Pic}></img>
     <Button type="primary">Primary</Button>
+    <Switch defaultChecked />
   </div>,
   document.getElementById("root")
 );
--- src/print.js
+++
@@ -1,4 +0,0 @@
-console.log('加载啦！！！The print.js module has loaded! See the network tab in dev tools...');
-export default function printMe() {
-  console.log('Button Clicked: Here\'s "some text"!');
-}
